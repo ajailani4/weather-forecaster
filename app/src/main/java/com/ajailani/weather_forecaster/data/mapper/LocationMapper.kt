7cefb3c83b8947075c06ef0642ce1110345f6b40
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-06-14 23:37:12 +0700

    Update LocationMapper

--- app/src/main/java/com/ajailani/weather_forecaster/data/mapper/LocationMapper.kt
+++ app/src/main/java/com/ajailani/weather_forecaster/data/mapper/LocationMapper.kt
@@ -2,12 +2,13 @@ package com.ajailani.weather_forecaster.data.mapper
 
 import com.ajailani.weather_forecaster.data.remote.dto.LocationDto
 import com.ajailani.weather_forecaster.domain.model.Location
+import java.util.Locale
 
 fun LocationDto.toLocation() =
     Location(
         name = name,
         lat = lat,
         lon = lon,
-        country = country,
+        country = Locale("", country).displayCountry,
         state = state
     )
