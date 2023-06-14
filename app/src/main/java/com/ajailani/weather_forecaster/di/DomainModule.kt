43a4c43ad61cf89aaa1683c1740f2e55fde3bd2b
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-06-14 22:48:00 +0700

    Add GetLocationsUseCase to domainModule DI

--- app/src/main/java/com/ajailani/weather_forecaster/di/DomainModule.kt
+++ app/src/main/java/com/ajailani/weather_forecaster/di/DomainModule.kt
@@ -1,11 +1,12 @@
 package com.ajailani.weather_forecaster.di
 
+import com.ajailani.weather_forecaster.domain.use_case.GetLocationsUseCase
 import com.ajailani.weather_forecaster.domain.use_case.GetWeatherInfoUseCase
 import com.ajailani.weather_forecaster.domain.use_case.SyncWeatherInfoUseCase
 import org.koin.dsl.module
 
 val domainModule = module {
+    single { GetLocationsUseCase(get()) }
     single { GetWeatherInfoUseCase(get()) }
-
     single { SyncWeatherInfoUseCase(get()) }
 }
