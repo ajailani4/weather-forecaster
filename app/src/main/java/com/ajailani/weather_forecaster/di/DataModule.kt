7faad5397410928f2c8a74f126c523a1fe48d790
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-06-18 22:52:20 +0700

    Delete WeatherDao and WeatherDatabase

--- app/src/main/java/com/ajailani/weather_forecaster/di/DataModule.kt
+++ app/src/main/java/com/ajailani/weather_forecaster/di/DataModule.kt
@@ -1,8 +1,6 @@
 package com.ajailani.weather_forecaster.di
 
-import androidx.room.Room
 import com.ajailani.weather_forecaster.data.local.PreferencesDataStore
-import com.ajailani.weather_forecaster.data.local.database.WeatherDatabase
 import com.ajailani.weather_forecaster.data.local.data_source.WeatherLocalDataSource
 import com.ajailani.weather_forecaster.data.remote.api_service.WeatherService
 import com.ajailani.weather_forecaster.data.remote.data_source.WeatherRemoteDataSource
@@ -14,17 +12,6 @@ import org.koin.dsl.module
 val dataModule = module {
     // API Service
     single { WeatherService(get()) }
-
-    // Database
-    single {
-        Room.databaseBuilder(
-            androidContext(),
-            WeatherDatabase::class.java,
-            "weather_db"
-        ).build()
-    }
-
-    single { get<WeatherDatabase>().weatherDao }
 
     // DataSource
     single { PreferencesDataStore(androidContext()) }
