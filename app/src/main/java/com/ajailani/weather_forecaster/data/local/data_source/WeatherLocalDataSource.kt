bba1ae4883727c770687e8424ce0e017b7e77242
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-06-18 23:05:08 +0700

    Update WeatherLocalDataSource

--- app/src/main/java/com/ajailani/weather_forecaster/data/local/data_source/WeatherLocalDataSource.kt
+++ app/src/main/java/com/ajailani/weather_forecaster/data/local/data_source/WeatherLocalDataSource.kt
@@ -1,11 +1,28 @@
 package com.ajailani.weather_forecaster.data.local.data_source
 
-import com.ajailani.weather_forecaster.data.local.dao.WeatherDao
+import com.ajailani.weather_forecaster.WeatherForecasterDatabase
 import com.ajailani.weather_forecaster.data.local.entity.WeatherInfoEntity
+import com.ajailani.weather_forecaster.data.mapper.toWeatherInfo
+import kotlinx.coroutines.flow.flowOf
 
-class WeatherLocalDataSource(private val weatherDao: WeatherDao) {
-    suspend fun insertWeatherInfo(weatherInfoEntity: WeatherInfoEntity) =
-        weatherDao.insertWeatherInfo(weatherInfoEntity)
+class WeatherLocalDataSource(weatherForecasterDatabase: WeatherForecasterDatabase) {
+    private val weatherInfoQueries = weatherForecasterDatabase.weatherInfoQueries
 
-    fun getWeatherInfo() = weatherDao.getWeatherInfo()
+    fun insertWeatherInfo(weatherInfoEntity: database.WeatherInfoEntity) {
+        weatherInfoQueries.insertWeatherInfo(
+            main = weatherInfoEntity.main,
+            description = weatherInfoEntity.description,
+            temp = weatherInfoEntity.temp,
+            feelsLike = weatherInfoEntity.feelsLike,
+            humidity = weatherInfoEntity.humidity,
+            speed = weatherInfoEntity.speed
+        )
+    }
+
+    fun getWeatherInfo() =
+        flowOf(
+            weatherInfoQueries
+                .getWeatherInfo()
+                .executeAsOneOrNull()
+        )
 }
